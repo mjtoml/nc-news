3c5a14ac24abc6902acfcae84b2fc984c1cf8377
Matt <37560967+mjtoml@users.noreply.github.com>
2022-11-14 13:36:30 +0000

    Add left join to avoid missed articles

--- __tests__/articles.test.js
+++ __tests__/articles.test.js
@@ -12,19 +12,6 @@ afterAll(() => {
   return db.end();
 });
 
-// Responds with:
-
-// - an `articles` array of article objects, each of which should have the following properties:
-//   - `author` which is the `username` from the users table
-//   - `title`
-//   - `article_id`
-//   - `topic`
-//   - `created_at`
-//   - `votes`
-//   - `comment_count` which is the total count of all the comments with this article_id - you should make use of queries to the database in order to achieve this.
-
-// - the articles should be sorted by date in descending order.
-
 describe("/api/articles", () => {
   describe("GET", () => {
     test("responds with an array of article objects, each with the expected properties", () => {
--- models/articles.js
+++ models/articles.js
@@ -3,7 +3,7 @@ const db = require("../db/connection");
 exports.selectArticles = () => {
   return db
     .query(
-      "SELECT articles.*, COUNT(comment_id)::int as comment_count FROM articles JOIN comments USING (article_id) GROUP BY article_id ORDER BY created_at DESC;"
+      "SELECT articles.*, COUNT(comment_id)::int as comment_count FROM articles LEFT JOIN comments USING (article_id) GROUP BY article_id ORDER BY created_at DESC;"
     )
     .then((articles) => {
       return articles.rows;
