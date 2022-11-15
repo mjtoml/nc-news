2ac4de5b783ef6cdb12afb5ea887c99e235bfb1c
Matt <37560967+mjtoml@users.noreply.github.com>
2022-11-15 10:24:12 +0000

    Implement PATCH article

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,4 +1,8 @@
-const { selectArticles, selectArticleById } = require("../models/articles");
+const {
+  selectArticles,
+  selectArticleById,
+  updateArticleById,
+} = require("../models/articles");
 
 exports.getArticles = (req, res, next) => {
   selectArticles()
@@ -11,6 +15,18 @@ exports.getArticles = (req, res, next) => {
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   selectArticleById(article_id)
+    .then((article) => {
+      if (!article) throw { status: 404, msg: "Article not found" };
+      res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.patchArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  const { inc_votes } = req.body;
+  if (!inc_votes) return next({ status: 400, msg: "Bad request" });
+  updateArticleById(article_id, inc_votes)
     .then((article) => {
       if (!article) throw { status: 404, msg: "Article not found" };
       res.status(200).send({ article });
